fe5751eb0232fda0f7edf3fcc306aaf88f4651df
Md. A. Barik <115731019+mrbarik@users.noreply.github.com>
2022-12-11 01:40:56 +0600

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -316,3 +316,118 @@ int main() {
    3   1   0   5
    5   3   2   0
 */
+
+//8. Merge sort in C++
+#include <iostream>
+using namespace std;
+void merge(int arr[], int p, int q, int r) {
+  // Create L ← A[p..q] and M ← A[q+1..r]
+  int n1 = q - p + 1;
+  int n2 = r - q;
+
+  int L[n1], M[n2];
+
+  for (int i = 0; i < n1; i++)
+    L[i] = arr[p + i];
+  for (int j = 0; j < n2; j++)
+    M[j] = arr[q + 1 + j];
+  int i, j, k;
+  i = 0;
+  j = 0;
+  k = p;
+  while (i < n1 && j < n2) {
+    if (L[i] <= M[j]) {
+      arr[k] = L[i];
+      i++;
+    } else {
+      arr[k] = M[j];
+      j++;
+    }
+    k++;
+  }
+  while (i < n1) {
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
+  while (j < n2) {
+    arr[k] = M[j];
+    j++;
+    k++;
+  }
+}
+
+void mergeSort(int arr[], int l, int r) {
+  if (l < r) {
+    int m = l + (r - l) / 2;
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+    merge(arr, l, m, r);
+  }
+}
+
+void printArray(int arr[], int size) {
+  for (int i = 0; i < size; i++)
+    cout << arr[i] << " ";
+  cout << endl;
+}
+
+int main() {
+  int arr[] = {6, 5, 12, 10, 9, 1};
+  int size = sizeof(arr) / sizeof(arr[0]);
+  mergeSort(arr, 0, size - 1);
+  cout << "Sorted array: \n";
+  printArray(arr, size);
+  return 0;
+}
+
+// 9. Quick sort in C++
+#include <iostream>
+using namespace std;
+
+void swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
+void printArray(int array[], int size) {
+  int i;
+  for (i = 0; i < size; i++)
+    cout << array[i] << " ";
+  cout << endl;
+}
+
+int partition(int array[], int low, int high) {
+  int pivot = array[high];
+  int i = (low - 1);
+  for (int j = low; j < high; j++) {
+    if (array[j] <= pivot) {
+      i++;
+      swap(&array[i], &array[j]);
+    }
+  }
+  swap(&array[i + 1], &array[high]);
+  return (i + 1);
+}
+
+void quickSort(int array[], int low, int high) {
+  if (low < high) {
+    int pi = partition(array, low, high);
+    quickSort(array, low, pi - 1);
+    quickSort(array, pi + 1, high);
+  }
+}
+
+int main() {
+  int data[] = {8, 7, 6, 1, 0, 9, 2};
+  int n = sizeof(data) / sizeof(data[0]);
+  cout << "Unsorted Array: \n";
+  printArray(data, n);
+  
+  quickSort(data, 0, n - 1);
+  
+  cout << "Sorted array in ascending order: \n";
+  printArray(data, n);
+}
+
+
